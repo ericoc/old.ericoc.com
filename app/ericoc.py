18d8544f5832f58d87c7cf0fad6520286d2e5100
Eric OC <eric@ericoc.com>
2018-06-14 19:13:57 +0000

    make it easier to reload uwsgi for development/testing and play with dictionary for url

--- app/ericoc.py
+++ app/ericoc.py
@@ -1,26 +1,23 @@
 from flask import Flask, request
 from pprint import pprint
 
-#pages = { 'projects': 'Projects', 'about': 'About', 'resume': 'Résumé' }
-#default_page = 'about'
+pages = { 'projects': 'Projects', 'about': 'About', 'resume': 'Résumé' }
 
 ericoc = Flask(__name__)
+
 
 @ericoc.route('/')
 def index():
     return "Hello, world!"
 
 
-@ericoc.route('/hello/<name>')
-def hello(name):
+@ericoc.route('/<page>/')
+def jawn(page):
 
-    if name == 'test':
-#        pprint(request)
-        print(request.__dict__)
-        return "stuff: %s" % format(request)
-
-    else:
-        return "Hello, {}!".format(name)
+        if page in pages:
+            return "Hello, {}!".format(pages[page])
+        else:
+            return "Not found!", 404
 
 if __name__ == "__main__":
     ericoc.run() # this is never run
