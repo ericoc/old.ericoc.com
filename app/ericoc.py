8345ced74a522a78ccac2375fcc2b1c7f9e101e3
Eric OC <eric@ericoc.com>
2018-06-15 18:18:52 +0000

    redo the templating so there isn't crazy inheritance going on and make the main app simpler

--- app/ericoc.py
+++ app/ericoc.py
@@ -5,29 +5,27 @@ pages = { 'projects': 'Projects', 'about': 'About', 'resume': 'Résumé' }
 ericoc = Flask(__name__)
 
 
-def fill_page(page, title, code=200):
-    template = page + '.html.j2'
-    return render_template(template, page=page, title=title, pages=pages), code
+def jawn(page='about', title=None, code=200):
+    return render_template('index.html.j2', page=page, title=title, pages=pages), code
 
 
 @ericoc.route('/')
 def index():
-    default_page = 'about'
-    return fill_page(page=default_page, title=None)
+    return jawn()
 
 
 @ericoc.route('/<page>/')
-def jawn(page):
+def specific(page):
 
         if page in pages:
-            return fill_page(page=page, title=pages[page])
+            return jawn(page=page, title=pages[page])
         else:
             return page_not_found(page);
 
 
 @ericoc.errorhandler(404)
 def page_not_found(error):
-    return fill_page(page='404', title='Page Not Found', code=404)
+    return jawn(page='404', title='Page Not Found', code=404)
 
 
 if __name__ == "__main__":
