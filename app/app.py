71e5d47831b647b443b488b5045a443b43369ed7
Eric OC <eric@ericoc.com>
2018-06-19 22:43:26 +0000

    rename to just 'app' and take out the nginx/uwsgi files since i keep changing them

--- app/app.py
+++ app/app.py
@@ -2,16 +2,16 @@ from flask import Flask, render_template, send_from_directory, request
 
 pages = { 'projects': 'Projects', 'about': 'About', 'resume': 'Résumé' }
 
-ericoc = Flask(__name__)
+app = Flask(__name__)
 
 def jawn(page='about', title=None, code=200):
     return render_template('index.html.j2', page=page, title=title, pages=pages), code
 
-@ericoc.route('/')
+@app.route('/')
 def index():
     return jawn()
 
-@ericoc.route('/<page>/')
+@app.route('/<page>/')
 def specific(page):
 
         if page in pages:
@@ -19,16 +19,16 @@ def specific(page):
         else:
             return page_not_found(page);
 
-@ericoc.route('/robots.txt')
-@ericoc.route('/sitemap.xml')
-@ericoc.route('/favicon.ico')
-@ericoc.route('/icon.png')
+@app.route('/robots.txt')
+@app.route('/sitemap.xml')
+@app.route('/favicon.ico')
+@app.route('/icon.png')
 def static_from_root():
-    return send_from_directory(ericoc.static_folder, request.path[1:])
+    return send_from_directory(app.static_folder, request.path[1:])
 
-@ericoc.errorhandler(404)
+@app.errorhandler(404)
 def page_not_found(error):
     return jawn(page='404', title='Page Not Found', code=404)
 
 if __name__ == "__main__":
-    ericoc.run()
+    app.run()
