232329e172306906f645359884bb768d26628d77
Eric OCallaghan <eric.ocallaghan@jefferson.edu>
2019-05-03 08:47:14 -0400

    remove unnecessary middle-man function and add comments

--- app/app.py
+++ app/app.py
@@ -1,24 +1,34 @@
 from flask import Flask, render_template, send_from_directory, request, url_for
 
-pages = { 'projects': 'Projects', 'about': 'About', 'resume': 'Résumé' }
+# List of valid pages and their proper titles
+pages = {
+            'projects'  :   'Projects',
+            'about'     :   'About',
+            'resume'    :   'Résumé'
+        }
 
+# Initialize Flask
 app = Flask(__name__)
 
-def jawn(page='about', title=None, code=200):
+# Define function to render Jinja2 template for all pages
+# Logic within the Jinja2 template includes which ever pages content .html file
+@app.route('/')
+def index(page='about', title=None, code=200):
     return render_template('index.html.j2', page=page, title=title, pages=pages), code
 
-@app.route('/')
-def index():
-    return jawn()
-
+# Define function to handle page requests if they are valid routes
 @app.route('/<page>/')
 def specific(page):
 
+        # Only pass page information to the main template function if it is a valid page from our list
         if page in pages:
             return jawn(page=page, title=pages[page])
+
+        # Otherwise, pass the page information to the 404 error handler
         else:
             return page_not_found(page)
 
+# Serve some static files
 @app.route('/robots.txt')
 @app.route('/sitemap.xml')
 @app.route('/favicon.ico')
@@ -26,9 +36,11 @@ def specific(page):
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
+# Handle 404 Not Found using a custom page with the main template function
 @app.errorhandler(404)
 def page_not_found(error):
-    return jawn(page='404', title='Page Not Found', code=404)
+    return index(page='404', title='Page Not Found', code=404)
 
+# Run Flask
 if __name__ == "__main__":
     app.run()
